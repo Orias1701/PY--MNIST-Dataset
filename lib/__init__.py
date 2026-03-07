ed8699a74f62ab06ab05c5641b0e9c5df3f15e26
Orias <long171.d.shine@gmail.com>
2026-03-07 21:26:13 +0700

    Add initial project structure with requirements and library support for MNIST dataset

---
+++ lib/__init__.py
@@ -0,0 +1,29 @@
+"""Thư viện hỗ trợ làm việc với MNIST dataset (OOP)."""
+
+from lib.dataset import MNISTDataLoader
+from lib.model import MNISTClassifier
+from lib.reduction import DimensionalityReducer
+from lib.utils import (
+    plot_samples,
+    plot_training_history,
+    plot_confusion_matrix,
+    print_classification_report,
+    measure_array_memory_mb,
+    run_and_measure_seconds,
+    plot_comparison_reduction,
+    print_comparison_table,
+)
+
+__all__ = [
+    "MNISTDataLoader",
+    "MNISTClassifier",
+    "DimensionalityReducer",
+    "plot_samples",
+    "plot_training_history",
+    "plot_confusion_matrix",
+    "print_classification_report",
+    "measure_array_memory_mb",
+    "run_and_measure_seconds",
+    "plot_comparison_reduction",
+    "print_comparison_table",
+]
